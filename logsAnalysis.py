7aca9efd126f8ce810b8c4273de25d54516b6dd0
cicomsa <ioanacomsa85@gmail.com>
2019-05-17 09:00:17 +0100

    converted to python 3

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python
 import psycopg2
-
-connection = psycopg2.connect("dbname=news")
-cursor = connection.cursor()
 
 
 def execute_query(query):
-    cursor.execute(query)
-    return cursor.fetchall()
+    try:
+        connection = psycopg2.connect("dbname=news")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.close()
+        return result
+    except psycopg2.Error as e:
+        print('Connection error')
+        print((e.pgerror))
+        print((e.diag.message_detail))
 
 
 def print_result(result):
     for i in range(len(result)):
-        print(str(i + 1) + '. ' + '"' + result[i][0] + '"' +
-              ' - ' + str(result[i][1]) + ' views')
+        return (('%s. "%s" - %s views' %
+                 (str(i + 1), result[i][0], str(result[i][1]))))
 
 
 def print_last_result(result):
@@ -21,23 +28,37 @@ def print_last_result(result):
     return (date + ' - ' + percent)
 
 
-query_1 = 'select articles.title, count(*) as num from articles, log where log.status like \'%200%\' and log.path ~ articles.slug group by articles.title order by num desc limit 3'
+query_1 = """
+    SELECT * \
+    FROM viewed_articles \
+    LIMIT 3 \
+"""
 
-query_2 = 'select authors.name, count(*) as num from authors, articles, log where log.status like \'%200%\' and authors.id = articles.author and log.path ~ articles.slug group by authors.name order by num desc limit 4'
+query_2 = """
+    SELECT name, sum(viewed_articles.views) AS views \
+    FROM article_authors, viewed_articles \
+    WHERE article_authors.title = viewed_articles.title \
+    GROUP BY name \
+    ORDER BY views desc \
+"""
 
-query_3 = 'select failedlogs.date, round(100.0*countfailedlogs/countalllogs,2) as percent from existinglogs, failedlogs where existinglogs.date = failedlogs.date and countfailedlogs*100 / countalllogs > 1'
+query_3 = """
+    SELECT date, percent \
+    FROM failed_logs \
+    GROUP BY date, \
+    percent HAVING percent > 1 \
+    ORDER BY percent \
+"""
 
 result_1 = execute_query(query_1)
 result_2 = execute_query(query_2)
 result_3 = execute_query(query_3)
 
 print('What are the most popular three  articles of all times?')
-print_result(result_1)
+print(print_result(result_1))
 print('--------------')
 print('Who are the most popular three  article authors of all times?')
-print_result(result_2)
+print(print_result(result_2))
 print('--------------')
 print('On which days did more than 1% of requests lead to errors?')
-print(print_last_result(result_3))
-
-connection.close()
+print((print_last_result(result_3)))
